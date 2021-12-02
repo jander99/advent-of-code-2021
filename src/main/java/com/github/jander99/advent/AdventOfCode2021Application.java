a32ed6910c60d62968cade07cef1eb007216ed52
Jeff Anderson <jander99@gmail.com>
2021-12-01 21:27:24 -0500

    Gradle SBoot Project

---
+++ src/main/java/com/github/jander99/advent/AdventOfCode2021Application.java
@@ -0,0 +1,13 @@
+package com.github.jander99.advent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventOfCode2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventOfCode2021Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jander99/advent/AdventOfCode2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jander99.advent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventOfCode2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
