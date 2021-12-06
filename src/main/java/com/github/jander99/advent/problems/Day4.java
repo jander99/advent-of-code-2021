bd0bc7df6f64c0712cb83c37d1028f21dd4babea
Jeff Anderson <jander99@gmail.com>
2021-12-05 20:17:10 -0500

    Day 4 Complete and Refactored to not suck ass

--- src/main/java/com/github/jander99/advent/problems/Day4.java
+++ src/main/java/com/github/jander99/advent/problems/Day4.java
@@ -1,58 +1,187 @@
 package com.github.jander99.advent.problems;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 
 @Slf4j
 public class Day4 {
 
-    private List<Integer> numbersToDraw;
-    private List<BingoBoard> bingoBoards;
-
-    public void fileParser(List<String> fileLines) {
-
+    public List<Integer> readGameNumbers(String numbers) {
+        return Arrays.stream(numbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
-    public BingoBoard buildBoard(List<String> boardLines) {
+    public List<BingoCard> populateBingoCards(List<String> numberBlocks) {
+        List<BingoCard> bingoCards = new ArrayList<>();
 
-        BingoBoard newBoard = new BingoBoard();
+        Iterator<String> numberBlockIterator = numberBlocks.iterator();
+        numberBlockIterator.next(); // Skip the game numbers
 
-        for (String line : boardLines) {
-
+        int numberBoards = 0;
+        while(numberBlockIterator.hasNext()) {
+            if (StringUtils.isNoneEmpty(numberBlockIterator.next())) {
+                continue; // Skip the empty lines between
+            }
+            List<String> oneBoardOfNumbers = new ArrayList<>();
+            for (int i=0;i<5;i++) {
+                oneBoardOfNumbers.add(numberBlockIterator.next());
+            }
+            BingoCard card = BingoCard.create(numberBoards, oneBoardOfNumbers);
+            bingoCards.add(card);
+            numberBoards++;
         }
 
-
-
-        return newBoard;
+        return bingoCards;
     }
 
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class BingoBoard {
+    public Pair<BingoCard, Integer> getFirstWinningCard(List<BingoCard> cards, List<Integer> numbers) {
+        return numbers.stream().map(number -> {
+            playNumber(number, cards);
+            return Pair.of(getWinningCard(cards),number);
+        })
+        .filter(bingo ->bingo.getKey().isPresent())
+        .map(bingo -> Pair.of(bingo.getKey().get(), bingo.getRight()))
+        .findFirst()
+        .orElseThrow();
+    }
 
-        private BingoCell[][] cells;
+    public Pair<BingoCard, Integer> getLastWinningCard(List<BingoCard> cards, List<Integer> numbers) {
+        List<BingoCard> playingCards = new ArrayList<>();
+        List<Pair<BingoCard, Integer>> completedCards = new ArrayList<>();
 
-        public void setCell(int x, int y, int value, boolean marked) {
-            cells[x][y] = BingoCell.builder().value(value).marked(marked).build();
+        playingCards.addAll(cards);
+
+        for (Integer number : numbers) {
+            if (completedCards.size() == cards.size()) {
+                continue;
+            }
+
+            playNumber(number, playingCards);
+
+            List<BingoCard> cardsThatJustWon = getWinningCards(playingCards);
+
+            if (cardsThatJustWon.size() > 0) {
+                cardsThatJustWon.forEach(card -> {
+                    completedCards.add(Pair.of(card, number));
+                    playingCards.remove(card);
+                });
+            }
         }
 
-        public boolean isCellMarked(int x, int y) {
-            return cells[x][y].isMarked();
-        }
+        return completedCards.get(completedCards.size() - 1);
     }
 
-    @Data
-    @Builder
-    public static class BingoCell {
-        private int value;
-        private boolean marked = false;
+    private void playNumber(Integer number, List<BingoCard> cards) {
+        cards.forEach(card -> card.stampCard(number));
     }
+
+    private Optional<BingoCard> getWinningCard(List<BingoCard> cards) {
+        return cards.stream()
+                .filter(card -> card.hasWinningRow().isPresent() || card.hasWinningColumn().isPresent())
+                .findFirst();
+    }
+
+    private List<BingoCard> getWinningCards(List<BingoCard> cards) {
+        return cards.stream()
+                .filter(card -> card.hasWinningRow().isPresent() || card.hasWinningColumn().isPresent())
+                .collect(Collectors.toList());
+    }
+}
+
+@Slf4j
+@Data
+class BingoCard {
+
+    private int cardId;
+
+    List<List<Pair<Integer, Boolean>>> elements = new ArrayList<>();
+
+    public static BingoCard create(int boardId, List<String> lines) {
+        assert lines.size() == 5;
+
+        BingoCard bingoCard = new BingoCard();
+
+        bingoCard.setCardId(boardId);
+
+        AtomicInteger columnPos = new AtomicInteger(0);
+        AtomicInteger rowPos = new AtomicInteger(0);
+
+
+        lines.forEach(line -> {
+            columnPos.set(0);
+
+            Arrays.stream(line.split(" "))
+                    .filter(StringUtils::isNoneEmpty)
+                    .map(Integer::parseInt)
+                    .forEach(number -> {
+                        if(columnPos.getAndIncrement() == 0) {
+                            bingoCard.elements.add(new ArrayList<>());
+                        }
+
+                        List<Pair<Integer, Boolean>> row =
+                                bingoCard.elements.get(rowPos.get());
+
+                        row.add(MutablePair.of(number, false));
+                    });
+            rowPos.incrementAndGet();
+        });
+        return bingoCard;
+    }
+
+    public void stampCard(int number) {
+        this.elements.forEach(row -> {
+            row.forEach(element -> {
+                if (element.getKey().equals(number)) {
+                    element.setValue(true);
+                }
+            });
+        });
+    }
+
+
+    public Optional<List<Integer>> hasWinningRow() {
+        return hasWinningRow(this.elements);
+    }
+
+    /** I should be able to flip the "matrix" and then check for rows now. */
+    public Optional<List<Integer>> hasWinningColumn() {
+
+        List<List<Pair<Integer, Boolean>>> flippedElements = IntStream.range(0, elements.get(0).size())
+                . mapToObj(col -> elements
+                        .stream()
+                        .map(e -> e.get(col))
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList());
+
+        return hasWinningRow(flippedElements);
+    }
+
+    /** Overload this so that I can get columns from rows */
+    private Optional<List<Integer>> hasWinningRow(List<List<Pair<Integer, Boolean>>> elements) {
+        return elements.stream().map(row ->
+                        row.stream().filter(Pair::getValue).map(Pair::getKey).collect(Collectors.toList()))
+                .filter(row -> row.size() == 5).findFirst();
+    }
+
+    public int getSumOfUnstampedElements() {
+        return elements
+                .stream()
+                .flatMap(Collection::stream)
+                .filter(p -> !p.getValue())
+                .map(Pair::getKey)
+                .mapToInt(i->i)
+                .sum();
+    }
+
 
 }
--- src/test/java/com/github/jander99/advent/problems/Day4Tests.java
+++ src/test/java/com/github/jander99/advent/problems/Day4Tests.java
@@ -1,11 +1,16 @@
 package com.github.jander99.advent.problems;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 public class Day4Tests {
 
     private static final String TEST_INPUT_FILE = "day4/test.txt";
@@ -20,9 +25,98 @@ public class Day4Tests {
         REAL_INPUT = InputLoader.readLinesFromInputFile(REAL_INPUT_FILE);
     }
 
+    @Test
+    public void sanityCheck() {
+
+        List<String> sampleBoard = List.of("1  2 3 4 5", "2 3 14 5 6",
+                "3 4 5 6  7", " 4 5 6 7 8","5 6 7 8  9");
+
+        BingoCard card = BingoCard.create(99, sampleBoard);
+
+        assertThat(card.hasWinningRow().isPresent()).isFalse();
+        assertThat(card.hasWinningColumn().isPresent()).isFalse();
+
+        card.stampCard(3);
+        card.stampCard(4);
+        card.stampCard(5);
+        card.stampCard(6);
+        card.stampCard(7);
+
+        assertThat(card.hasWinningRow().isPresent()).isTrue();
+        assertThat(card.hasWinningColumn().isPresent()).isFalse();
+        assertThat(card.hasWinningRow().get()).containsExactly(3,4,5,6,7);
+
+        card.stampCard(8);
+
+        assertThat(card.hasWinningRow().isPresent()).isTrue();
+        assertThat(card.hasWinningRow().get()).containsExactly(3,4,5,6,7);
+        assertThat(card.hasWinningColumn().isPresent()).isTrue();
+        assertThat(card.hasWinningColumn().get()).containsExactly(4,5,6,7,8);
+    }
 
     @Test
     public void partOne() {
+
+        Day4 day4 = new Day4();
+        List<Integer> numbersToDraw = day4.readGameNumbers(TEST_INPUT.get(0));
+        List<BingoCard> bingoCards = day4.populateBingoCards(TEST_INPUT);
+
+        assertThat(bingoCards).hasSize(3);
+
+        Pair<BingoCard, Integer> firstWinner = day4.getFirstWinningCard(bingoCards, numbersToDraw);
+        BingoCard card = firstWinner.getKey();
+        int winningNumber = firstWinner.getValue();
+        int sumOfABitch = card.getSumOfUnstampedElements();
+
+        assertThat(card.hasWinningRow().isPresent()).isTrue();
+        assertThat(card.hasWinningRow().get()).containsExactly(14,21,17,24,4);
+        assertThat(winningNumber).isEqualTo(24);
+        assertThat(sumOfABitch).isEqualTo(188);
+
+
+        numbersToDraw = day4.readGameNumbers(REAL_INPUT.get(0));
+        bingoCards = day4.populateBingoCards(REAL_INPUT);
+
+
+        firstWinner = day4.getFirstWinningCard(bingoCards, numbersToDraw);
+        card = firstWinner.getKey();
+        winningNumber = firstWinner.getValue();
+        sumOfABitch = card.getSumOfUnstampedElements();
+
+        int answer = winningNumber * sumOfABitch;
+
+        log.info("Day 4, Part 1: {}", answer);
+    }
+
+    @Test
+    public void partTwo() {
+
+        Day4 day4 = new Day4();
+        List<Integer> numbersToDraw = day4.readGameNumbers(TEST_INPUT.get(0));
+        List<BingoCard> bingoCards = day4.populateBingoCards(TEST_INPUT);
+
+        assertThat(bingoCards).hasSize(3);
+
+        Pair<BingoCard, Integer> lastWinner = day4.getLastWinningCard(bingoCards, numbersToDraw);
+        BingoCard card = lastWinner.getKey();
+        int winningNumber = lastWinner.getValue();
+        int sumOfABitch = card.getSumOfUnstampedElements();
+
+        assertThat(winningNumber).isEqualTo(13);
+        assertThat(sumOfABitch).isEqualTo(148);
+
+        numbersToDraw = day4.readGameNumbers(REAL_INPUT.get(0));
+        bingoCards = day4.populateBingoCards(REAL_INPUT);
+
+
+        lastWinner = day4.getLastWinningCard(bingoCards, numbersToDraw);
+        card = lastWinner.getKey();
+        winningNumber = lastWinner.getValue();
+        sumOfABitch = card.getSumOfUnstampedElements();
+
+        int answer = winningNumber * sumOfABitch;
+
+        log.info("Day 4, Part 2: {}", answer);
 
     }
 }
